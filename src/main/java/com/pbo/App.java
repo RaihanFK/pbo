9afd5b61af5e202fdba0fc6b0a719e8e38c9f9f7
youkwhd <lolywk@tutanota.com>
2024-11-02 01:59:11 +0700

    chore: configure trailing slash

--- src/main/java/com/pbo/App.java
+++ src/main/java/com/pbo/App.java
@@ -2,19 +2,20 @@ package com.pbo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
+@RestController
 public class App {
 
-	@RequestMapping("/")
-	String home() {
-		return "Hello Java && Spring!";
-	}
+    @GetMapping()
+    private String home() {
+        return "Hello from Java && Spring!";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 }
---
+++ src/main/java/com/pbo/WebConfiguration.java
@@ -0,0 +1,14 @@
+package com.pbo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseTrailingSlashMatch(true);
+    }
+}
