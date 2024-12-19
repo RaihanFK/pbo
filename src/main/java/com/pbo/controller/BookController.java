b1b6f9243c202b4df464f1d71a2eaaef5632166e
youkwhd <lolywk@tutanota.com>
2024-12-20 00:51:12 +0700

    chore(book): add /{bookId}

--- src/main/java/com/pbo/controller/BookController.java
+++ src/main/java/com/pbo/controller/BookController.java
@@ -1,21 +1,20 @@
 package com.pbo.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import com.pbo.model.Book;
 import com.pbo.repository.BookRepository;
@@ -34,6 +33,13 @@ public class BookController {
     @GetMapping
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
+    }
+    
+    @GetMapping("/{bookId}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long bookId) {
+        return bookRepository.findById(bookId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build()); // TODO(youkwhd): 404 notFound isn't quite right: https://developer.x.com/en/support/x-api/error-troubleshooting
     }
 
     @GetMapping("/recommendation")
